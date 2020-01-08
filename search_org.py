5ced4c1596d54bb9cd0eaf2c231f310ba5cb1350
dsespitia <dsespitia@gmail.com>
2020-01-08 17:52:55 -0500

    Update search_org.py

--- search_org.py
+++ search_org.py
@@ -127,7 +127,7 @@ def search_org(api_shodan, ips, args):
 
 def main():
     """ Se configura la llave de la API de Shodan"""
-    api_shodan = "FLI4oMfAVAb6IyYbtO4e9lvj7DlI5BM5"
+    api_shodan = ""
     args = argumentos()
     if args.dominio == None:
         print(parser.print_usage)
